b25915b3dd439b16012b8104c690ba18ae527447
MarcosVB <mvberte@hotmail.com>
2025-01-08 20:28:58 -0300

    fix: ensure fixed values are kept when cloning

--- src/Row.ts
+++ src/Row.ts
@@ -2,21 +2,21 @@ export class Row {
   private readonly row: number[];
   private readonly fixed: Set<number>;
 
-  constructor(row: number[]) {
+  constructor(row: number[], fixed?: Set<number>) {
     if (row.length !== 9) {
       throw new Error(`Invalid row size: ${row.length}`);
     }
     this.row = row;
-    this.fixed = new Set();
+    this.fixed = fixed ?? new Set();
     const availableNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      .sort(() => Math.random() - 0.5)
-      .filter((value) => row.indexOf(value) === -1);
+      .filter((value) => row.indexOf(value) === -1)
+      .sort(() => Math.random() - 0.5);
     this.row.forEach((value, index) => {
       if (value < 0 || value > 9) {
         throw new Error(`Invalid input: ${value}`);
       }
       if (value !== 0) {
-        this.fixed.add(index);
+        !fixed && this.fixed.add(index);
       } else {
         this.row[index] = availableNumbers.shift()!;
       }
@@ -31,18 +31,14 @@ export class Row {
     return this.row[index];
   }
 
-  public isFixed(index: number) {
-    return this.fixed.has(index);
-  }
-
   public swap() {
     // Considering a 9x9 board
     if (this.fixed.size > 7) {
       return;
     }
     const swappableIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-      .sort(() => Math.random() - 0.5)
-      .filter((index) => !this.fixed.has(index));
+      .filter((index) => !this.fixed.has(index))
+      .sort(() => Math.random() - 0.5);
     const [base, target] = swappableIndexes;
     const temp = this.row[base];
     this.row[base] = this.row[target];
@@ -50,8 +46,6 @@ export class Row {
   }
 
   public clone() {
-    return new Row(
-      this.row.map((value, index) => (this.isFixed(index) ? value : 0))
-    );
+    return new Row([...this.row], new Set([...this.fixed]));
   }
 }
